e31e6e09e177af93b5cbfb7558bcafab5977637a
Jonas Reich <joreich@stud.mediadesign.de>
2017-04-21 23:28:44 +0100

    Delete readme from handover + cleaned up batch files

--- bomber_clone_tcp/server_client.bat
+++ bomber_clone_tcp/server_client.bat
@@ -1,19 +1,14 @@
-
 ::@echo off
 if [%1%]==[] (
-	set "map=cross"
+	set "map=dijkstra"
 ) else (
 	set "map=%1"
 )
-
 
 :: server
 start %~dp0bin\Release\bomber_clone_server\bomber_clone_server dat\%map%.ppm 1 2000
 
 timeout 1
 
-:: cerberus
-:: start C:\Users\joreich\vorlesung_softwaretechnik\main\BomberCloneServer\bin\cerb3rus_ai_Release.exe
-
-:: my bot
+:: client
 %~dp0bin\Release\bomber_clone_client\bomber_clone_client localhost 2000 bot clear
